823211b48fe440d82c3c18bd4148b4db334163e2
Swapnil <swapnilacharjee04@gmail.com>
2024-03-31 15:02:06 +0530

    Create a README.md file

--- pages/Record_a_journal.py
+++ pages/Record_a_journal.py
@@ -1,4 +1,6 @@
 import streamlit as st
+from send_email import send_email
+import time
 
 
 st.set_page_config(layout="wide")
@@ -13,4 +15,20 @@ with col10:
 
         st.markdown("<h1 style='text-align: center;'>Make a journal of how you are feeling</h1>",
                     unsafe_allow_html=True)
+        receiver = st.text_input("Enter your email Id: ")
+        raw_message = st.text_area("Briefly describe how you feel")
 
+        message =f"""\
+Subject:{time.strftime("DATE - %d %b, %Y; TIME - %H:%M")}
+
+From: Harmony app
+Recorded Journal: {raw_message}
+        """
+
+        submit_button = st.form_submit_button("Submit and send to your email")
+
+        if submit_button:
+            send_email(message, receiver)
+            st.info("""
+            Thank you for recording a journal with Harmony!
+            """)
